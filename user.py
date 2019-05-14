f2099a78ccbaf7a0fca58598329af88bdc52b8f2
Attila Wiedemann <attila.wiedemann@gmail.com>
2019-05-14 13:37:00 +0200

    add get inputs to user.py

--- user.py
+++ user.py
@@ -3,4 +3,10 @@ def make_table(table, title_list):
     return table
 
 
-def 
+def get_inputs(list_labels, title):
+    inputs = []
+    print(f"{title}")
+    for item in list_labels:
+        user_input = input(f"{item}")
+        inputs.append(user_input)
+    return inputs

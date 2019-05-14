6f40578cab0289cb4c53cf9f8b27595e55d3311f
vnelli <varga.nelli9a@gmail.com>
2019-05-14 13:43:56 +0200

    make_table, print_table, print_menu in progress

--- user.py
+++ user.py
@@ -22,7 +22,17 @@ def len_of_columns(new_table):
     for lists in new_table:
         for item in lists:
             length_of_items.append(len(item))
-            
+    length_of_table = len_of_table(new_table)
+    row_to_column_list = [length_of_items[x:x+length_of_table] for x in range(0, len(length_of_items), length_of_table)]
+    longest_titles = []
+    for i in row_to_column_list:
+        longest_titles.append(max(i))
+    count = 0
+    for i in longest_titles:
+        design_width = 5
+        longest_titles[count] += design_width
+        count += 1
+    return longest_titles
 
 
 def print_table(table, title_list):
@@ -30,7 +40,26 @@ def print_table(table, title_list):
     new_table = transform_table(table)
     dash_chr = "="
     right_slash = "/"
-    left_slash = "\"
+    left_slash = "\\"
     separator = "||"
+    longest_titles = len_of_columns(new_table)
+    separator_line = []
+    for i in longest_titles:
+        separator.append(i*dash_chr+separator)
+        joint_separator_line = "".join(separator_line)
+        list_separator_line = separator + joint_separator_line
+        last_line = left_slash + (len(joint_separator_line)-1)*dash_chr+right_slash
+        first_line = right_slash + (len(joint_separator_line)-1)*dash_chr+left_slash
+    for lists in table:
+        if lists == table[0]:
+            pass
+        else:
+            print(f"\n{list_separator_line}")
+        print(separator, end="")
+    return table, title_list
 
-    
+    def print_menu(title, list_options, exit_message):
+        print(f'\t{title}:')
+        for i in range(len(list_options)):
+            print(f'\t\t({i+1}) {list_options[i]}')
+        print(f'\t\t(0) {exit_message}')
